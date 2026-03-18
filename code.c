d50fd1297b72bff974590264cc59fa77d978d1c4
unknown <2612130061@ACADEMICO.IESB.BR>
2026-03-18 10:59:25 -0300

    codigos da aula02

---
+++ code.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+
+int main(){
+
+printf("=================\n"); 
+printf("    BOLETIM DE NOTAS\n");
+printf("=================/n");
+printf("Disciplina  A1 A2 A3 MF\n");
+printf("APC 1   5.2 2.3 6.0 5.6\n");
+printf("MATEMATICA DISC 6.0 5.0 - 5.5\n");
+printf("CALCULO 4.0 4.0 4.0 4.0\n"):
+
+return 0
+}
