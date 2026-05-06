f25a63cdb27278bf13f96347d5f96c6a12729a53
joao <joaovitormk9@gmail.com>
2026-05-06 10:55:07 -0300

    feat: conclui pratica06. Fecha #15

--- praticas/pratica06/dia_da_semana.c
+++ praticas/pratica06/dia_da_semana.c
@@ -4,17 +4,28 @@ int main (){
 
     int dia;
 
-    printf("Digite um dia da semana1 a 7: \n", dia);
+    printf("Digite um dia da semana de 1 a 7: \n");
     scanf("%i", &dia);
 
-    if (dia = 1 || dia == 7) {
-        printf("Final de semana\n", dia);
-    }
-    else if (dia == 2 || dia == 3 || dia == 4 || dia == 5 || dia == 6) {
-        printf("Dia util\n", dia);
-    }
-    else {
-        printf("naum eh um dia da semana\n", dia);
+    // // if (dia = 1 || dia == 7) {
+    // //     printf("Final de semana\n", dia);
+    // // }
+    // // else if (dia == 2 || dia == 3 || dia == 4 || dia == 5 || dia == 6) {
+    // //     printf("Dia util\n", dia);
+    // // }
+    // // else {
+    // //     printf("naum eh um dia da semana\n", dia);
+    // }
+
+    switch (dia) {
+        case 1: printf("Domingo, Final de semana\n"); break;
+        case 2: printf("Segunda-Feira, Dia útil\n"); break;
+        case 3: printf("Terça-Feira, Dia útil\n"); break;
+        case 4: printf("Quarta-Feira, Dia útil\n"); break;
+        case 5: printf("Quinta-Feira, Dia útil\n"); break;
+        case 6: printf("Sexta-Feira, Dia útil\n"); break;
+        case 7: printf("Sábado, Final de semana\n"); break;
+        default: printf("Não é um dia da semana\n");
     }
 
 
